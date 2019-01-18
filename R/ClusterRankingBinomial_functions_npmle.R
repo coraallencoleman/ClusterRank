263766b051f6e2ab5059a8265daecfc621337d08
Cora Allen-Coleman <6508099+coraallencoleman@users.noreply.github.com>
2019-01-18 15:41:28 -0600

    meeting notes

--- R/ClusterRankingBinomial_functions_npmle.R
+++ R/ClusterRankingBinomial_functions_npmle.R
@@ -6,44 +6,50 @@ library(reshape2)
 library(clue)
 library(Hmisc)
 library(RColorBrewer)
-
+#TODO binomial data should be something else. use LBW for poisson
 npmle.bin <- function(y,n,k=NULL,n.iter=1000,row_names=NULL) {
+  #k is number of initial clusters
 #  theta <- quantile(y/n,probs=(0:(k-1))/(k-1))
   if (is.null(k)) {
     theta<-sort(y/n) #sorted probabilities
     k<-length(theta) #k = number of units to rank
   } else {
-    theta <- seq(min(y/n),max(y/n),length=k)
+    theta <- seq(min(y/n),max(y/n),length=k) #mass points of F We're estimating these
   }
-  p_theta <- rep(1/k,k) #evenly spaced probabilities between 0 and 1 for groups?
+  p_theta <- rep(1/k,k) #probabilities of each mass point. We're estimating these
 
-  E_z <- matrix(NA,length(y),k)
+  E_z <- matrix(NA,length(y),k) #expeted value of the probability that youre in each of hte groups
+  #calculating the p that zij is equal to theta star j
   for (j in 1:n.iter) {
     for (i in 1:k) {
+      #numerator
       E_z[,i] <- log(p_theta[i])+dbinom(y,n,theta[i],log=TRUE) #TODO E_z is log(pr associated with group) + log(quantile)?
     }
-    E_z <- t(apply(E_z,1,function(x) exp(x-max(x))/sum(exp(x-max(x))))) #E_z will be
-    p_theta <- apply(E_z,2,mean)
-    theta <- y%*%E_z/n%*%E_z
+    E_z <- t(apply(E_z,1,function(x) exp(x-max(x))/sum(exp(x-max(x))))) #normalizes (pseudo zs). This is the E part of EM alg
+    p_theta <- apply(E_z,2,mean) #M step: means over the
+    theta <- y%*%E_z/n%*%E_z #theta for each group
   }
-
+  #this reduces down to needed number of groups k
   ord<-order(theta)
-  theta<-c(theta[ord])
-  p_theta<-p_theta[ord]
+  theta<-c(theta[ord]) #sorts
+  p_theta<-p_theta[ord] #sorts
 
   p_theta <- tapply(p_theta,cumsum(!duplicated(round(theta,8))),sum)
+  #cumsum numbers groups is ascending order. sums the pthetas that goes with each group. See pictures
   theta <- theta[!duplicated(round(theta,8))]
 
   E_z <- matrix(NA,length(y),length(theta))
+  #final posterior probabilties for each county. Pr(county in group i)
   for (i in 1:length(theta)) {
     E_z[,i] <- log(p_theta[i])+dbinom(y,n,theta[i],log=TRUE)
   }
-  E_z <- t(apply(E_z,1,function(x) exp(x-max(x))/sum(exp(x-max(x)))))
+  E_z <- t(apply(E_z,1,function(x) exp(x-max(x))/sum(exp(x-max(x))))) #normalizes probabilities. subtract max to avoid underflow
 
   rownames(E_z)<-row_names
-  colnames(E_z)<-signif(theta,3)
+  colnames(E_z)<-signif(theta,3) #group names are rounded
 
   return(list(theta=theta, p_theta=p_theta, post_theta=E_z))
+  #(priors, posterior)
 }
 
 rank_cluster.bin <- function(y,n,k=NULL,scale=identity,weighted=TRUE,n.iter=1000,n.samp=10000,row_names=NULL) {
@@ -58,8 +64,8 @@ rank_cluster.bin <- function(y,n,k=NULL,scale=identity,weighted=TRUE,n.iter=1000
                theta=scale(npmle_res$theta),n.samp=n.samp)
   smp <- t(smp)
   smp.ord <- apply(smp,2,sort)
-  if (weighted)
-    wgt <- 1/pmax(.Machine$double.eps,apply(smp,1,var))
+  if (weighted) #inverse variance weighting
+    wgt <- 1/pmax(.Machine$double.eps,apply(smp,1,var)) #if variance is zero, uses v small value to make it impossible to reassign to new group
   else wgt <- rep(1,N)
 
   loss <- matrix(NA,N,N)
@@ -70,14 +76,15 @@ rank_cluster.bin <- function(y,n,k=NULL,scale=identity,weighted=TRUE,n.iter=1000
   }
 
   rnk <- as.numeric(clue::solve_LSAP(loss))
-  grp <- match(apply(smp.ord,1,getmode),scale(npmle_res$theta))[rnk]
+  grp <- match(apply(smp.ord,1,getmode),scale(npmle_res$theta))[rnk] #matches rank positions to groups using mode. The mode version minimizes indicator (see pic)
+  #^ We could replace this with squared error diff. See pic.
   grp <- factor(grp)
   p_grp <- npmle_res$post_theta[cbind(1:N,as.numeric(grp))]
-  levels(grp) <- signif(npmle_res$theta,3)
+  levels(grp) <- signif(npmle_res$theta,3) #labels
 
   ord <- order(rnk)
 
-  CI <- Hmisc::binconf(y,n)
+  CI <- Hmisc::binconf(y,n) #creating confidence intervals
 
   ranked_table <- data_frame(name=row_names,rank=rnk,group=factor(grp),
                              y=y,n=n,p=y/n,
